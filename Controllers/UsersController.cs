188e07c2e83595718009379470cb85a45a048869
erp1 <erp1@JVC-ERP1>
2025-06-04 15:03:39 +0530

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SqlServerApi.Data;
+using SqlServerApi.Models;
+using Microsoft.EntityFrameworkCore;
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public UsersController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<User>>> Get()
+    {
+        return await _context.Users.ToListAsync();
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SqlServerApi.Models;
+using System.Collections.Generic;
+namespace SqlServerApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
